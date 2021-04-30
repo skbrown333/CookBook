0163832af891183819ad48b455036b6e51ba9929
skbrown333 <skbrown333@gmail.com>
2021-04-30 02:43:13 -0500

    fix(MatchupDetailView): fix styling

--- cookbook/src/components/MatchupDetailView/MatchupDetailView.tsx
+++ cookbook/src/components/MatchupDetailView/MatchupDetailView.tsx
@@ -45,12 +45,12 @@ export const MatchupDetailView: FunctionComponent<MatchupDetailViewProps> = () =
     character: "falco",
     sections: [
       {
-        title: "# basics",
+        title: "basics",
         body: `The first key to understanding how to fight falco is that both of his primary walling options (bair and utilt) have virtually the exact same range. Meaning, if you're spacing for one you're simultaneously spacing for the other. This makes it far simpler to smother him/punish him
 \n![](https://media.giphy.com/media/ZpLzabCMomHUQPbcvg/giphy.gif) ![](https://media.giphy.com/media/ZpLzabCMomHUQPbcvg/giphy.gif) ![](https://media.giphy.com/media/ZpLzabCMomHUQPbcvg/giphy.gif)`,
       },
       {
-        title: "# percents",
+        title: "percents",
         body: `## \`No DI\`\n
 * \`40%\` Uthrow regrab
 * \`72%\` Uthrow fsmash
@@ -63,7 +63,7 @@ export const MatchupDetailView: FunctionComponent<MatchupDetailViewProps> = () =
 * \`~105%\` Uthrow dash SH knee`,
       },
       {
-        title: "# defense-and-recovery",
+        title: "defense-and-recovery",
         body: `![](https://media.giphy.com/media/ZpLzabCMomHUQPbcvg/giphy.gif) ![](https://media.giphy.com/media/ZpLzabCMomHUQPbcvg/giphy.gif) ![](https://media.giphy.com/media/ZpLzabCMomHUQPbcvg/giphy.gif)`,
       },
     ],
@@ -78,10 +78,14 @@ export const MatchupDetailView: FunctionComponent<MatchupDetailViewProps> = () =
       return {
         name: title,
         id: index,
-        onClick: () =>
-          document
-            .getElementById(title)
-            ?.scrollIntoView({ behavior: "smooth" }),
+        onClick: () => {
+          let div = document.getElementById(section.title);
+          if (div && document) {
+            let topPos = div.offsetTop - 200;
+            let sectionsDiv = document.getElementById("sections");
+            if (sectionsDiv) sectionsDiv.scrollTop = topPos;
+          }
+        },
       };
     });
   };
@@ -93,49 +97,54 @@ export const MatchupDetailView: FunctionComponent<MatchupDetailViewProps> = () =
     setMatchup({ ...matchup });
   };
 
+  const handleCancel = () => {
+    setMatchup({ ...mockMatchup });
+    setEditing(false);
+  };
+
+  const handleSave = () => {
+    setEditing(false);
+  };
+
   const buildSections = () => {
     if (!matchup) return;
 
     return matchup.sections.map((section, index) => {
       const { title, body } = section;
 
-      if (editing) {
-        return (
-          <EuiPanel
-            hasShadow={false}
-            hasBorder
-            className="matchup-detail__panel"
-          >
-            <EuiFieldText
-              className="matchup-detail__title-input"
-              placeholder="title"
-              value={title}
-              onChange={(e) => updateSection("title", e.target.value, index)}
-            />
-            <EuiMarkdownEditor
-              className="matchup-detail__body-input"
-              aria-label="Body markdown editor"
-              value={body}
-              onChange={(value) => updateSection("body", value, index)}
-              height={400}
-            />
-          </EuiPanel>
-        );
-      }
-
       return (
         <EuiPanel
-          id={title}
+          id={section.title}
           hasShadow={false}
           hasBorder
-          className="matchup-detail__panel"
+          className="matchup-section"
         >
-          <div className="matchup-detail__title">
-            <EuiMarkdownFormat>{`# **${title}** \n---`}</EuiMarkdownFormat>
-          </div>
-          <div className="matchup-detail__body">
-            <EuiMarkdownFormat>{body}</EuiMarkdownFormat>
-          </div>
+          {editing ? (
+            <>
+              <EuiFieldText
+                className="matchup-section__title-input"
+                placeholder="title"
+                value={title}
+                onChange={(e) => updateSection("title", e.target.value, index)}
+              />
+              <EuiMarkdownEditor
+                className="matchup-section__body-input"
+                aria-label="Body markdown editor"
+                value={body}
+                onChange={(value) => updateSection("body", value, index)}
+                height={400}
+              />
+            </>
+          ) : (
+            <>
+              <div className="matchup-section__title">
+                <EuiMarkdownFormat>{`# **\#** **${title}** \n---`}</EuiMarkdownFormat>
+              </div>
+              <div className="matchup-section__body">
+                <EuiMarkdownFormat>{body}</EuiMarkdownFormat>
+              </div>
+            </>
+          )}
         </EuiPanel>
       );
     });
@@ -145,53 +154,70 @@ export const MatchupDetailView: FunctionComponent<MatchupDetailViewProps> = () =
     <div id="matchup-detail" className="matchup-detail">
       {matchup && (
         <>
-          <EuiBreadcrumbs
-            breadcrumbs={breadcrumbs}
-            truncate={false}
-            className="matchup-detail__breadcrumbs"
-          />
-          <EuiTabs>
-            <EuiTab onClick={() => {}} key={"1"}>
-              Wiki
-            </EuiTab>
-            <EuiTab onClick={() => {}} key={"1"}>
-              Posts
-            </EuiTab>
-          </EuiTabs>
-          <EuiPanel
-            className="matchup-detail__side-nav"
-            hasShadow={false}
-            hasBorder
-          >
-            <EuiAvatar
-              size="xl"
-              name={character.name}
-              imageUrl={
-                "https://www.textures-resource.com/resources/sheet_icons/4/3300.png"
-              }
-            ></EuiAvatar>
-            <EuiSideNav
-              className="matchup-detail__nav-items"
-              aria-label="Basic example"
-              style={{ width: 192 }}
-              items={[
-                {
-                  name: character.name,
-                  id: 0,
-                  items: buildSideNaveItems(),
-                },
-              ]}
-            />
-            <EuiButton
-              className="matchup-detail__edit-button"
-              fill
-              iconType="pencil"
-              onClick={() => setEditing(!editing)}
+          <div className="matchup-detail__controls">
+            {editing ? (
+              <>
+                <EuiButton
+                  className="matchup-controls__button"
+                  fill
+                  iconType="trash"
+                  color="danger"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </EuiButton>
+                <EuiButton
+                  className="matchup-controls__button"
+                  fill
+                  iconType="save"
+                  color="secondary"
+                  onClick={handleSave}
+                >
+                  Save
+                </EuiButton>
+              </>
+            ) : (
+              <EuiButton
+                className="matchup-controls__button"
+                fill
+                iconType="pencil"
+                onClick={() => setEditing(!editing)}
+              >
+                Edit Page
+              </EuiButton>
+            )}
+          </div>
+          <div className="matchup-detail__content">
+            <EuiPanel
+              className="matchup-content__side-nav"
+              hasShadow={false}
+              hasBorder
             >
-              Edit Page
-            </EuiButton>
-          </EuiPanel>
-          {buildSections()}
+              <EuiAvatar
+                size="xl"
+                name={character.name}
+                imageUrl={
+                  "https://www.textures-resource.com/resources/sheet_icons/4/3300.png"
+                }
+              ></EuiAvatar>
+              <EuiSideNav
+                className="matchup-content__side-nav__items"
+                aria-label="Basic example"
+                style={{ width: 192 }}
+                items={[
+                  {
+                    name: character.name,
+                    id: 0,
+                    items: buildSideNaveItems(),
+                  },
+                ]}
+              />
+            </EuiPanel>
+            <div id="sections" className="matchup-content__sections">
+              {buildSections()}
+            </div>
+            <div className="matchup-content__right"></div>
+          </div>
         </>
       )}
     </div>
