3885c81c7747a1c38a7a1fba09ddb006fecb4c4e
skbrown333 <skbrown333@gmail.com>
2021-05-07 14:36:24 -0500

    [COOK-52] Make Detail Page Responsive (#15)
    * feat(GuideDetailView): add mobile buttons
    
    COOK-52
    
    * style(_guide-detail): fix button margin
    
    COOK-52
    
    * style(GuideDetailView): fix editor styling
    
    COOK-52
    
    * fix(GuideDetail): move twitch styling
    
    COOK-52
    
    * style(GuideDetail): fix style
    
    * style(GuideDetailView): change button styles
    
    * chore(GuideDetailView): only use icon buttons
    
    COOK-52
    
    * chore(GuideDetail): split up components
    
    * style(GuideDetailSideNav): clean up
    
    * fix(GuideDetailSideNav): fix links
    
    * fix(_guide-detail-side-nav): fix margin
    
    * style(GuideDetailSideNav): change avatar
    
    * style(_guide-detail-view): fix padding

---
+++ src/components/GuideDetailView/GuideDetailSideNav/GuideDetailSideNav.tsx
@@ -0,0 +1,73 @@
+import React, { FunctionComponent } from "react";
+
+/* Components */
+import { EuiAvatar, EuiPanel, EuiSideNav } from "@elastic/eui";
+import { Post } from "../../../models/Post";
+
+/* Styles */
+import "./_guide-detail-side-nav.scss";
+
+export interface GuideDetailSideNavProps {
+  editing: boolean;
+  title: string;
+  sections: Array<Post>;
+}
+
+export const GuideDetailSideNav: FunctionComponent<GuideDetailSideNavProps> = ({
+  editing,
+  title,
+  sections,
+}) => {
+  const buildSideNaveItems = () => {
+    if (!sections) return;
+    return sections.map((section, index) => {
+      const { title } = section;
+      return {
+        name: title,
+        id: index,
+        onClick: () => {
+          let div = document.getElementById(`section-${index}`);
+          if (div && document) {
+            let topPos = div.offsetTop - 200;
+            let sectionsDiv = document.getElementById("sections");
+            if (sectionsDiv) sectionsDiv.scrollTop = topPos;
+          }
+        },
+      };
+    });
+  };
+
+  return (
+    <EuiPanel
+      className="guide-content__side-nav"
+      style={editing ? { marginTop: 8 } : {}}
+      hasShadow={false}
+      hasBorder
+    >
+      <div className="side-nav__header">
+        <EuiAvatar
+          size="m"
+          className="side-nav__header__avatar"
+          name={title}
+          color={null}
+          imageUrl={"https://ssb.wiki.gallery/images/d/d6/FalcoHeadSSBM.png"}
+        ></EuiAvatar>
+        {title}
+      </div>
+      <div className="side-nav__content">
+        <EuiSideNav
+          className="side-nav-content__items"
+          aria-label="Basic example"
+          style={{ width: 192 }}
+          items={[
+            {
+              name: "Sections",
+              id: 0,
+              items: buildSideNaveItems(),
+            },
+          ]}
+        />
+      </div>
+    </EuiPanel>
+  );
+};
