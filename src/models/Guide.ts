8f50932d9cea584cbd24d035dd3c97e989e96283
Collin <cjdekalb@Gmail.com>
2021-05-15 13:48:03 -0500

    [COOK-67] Guides (#26)
    * refactor(App.tsx): added toast service to app
    
    * feat(firebase, App.tsx): added cookbook info function
    
    * chore(constants, Guide.ts): changed guide to include character
    
    * feat(GuideDetail): Guides link to their own pages
    
    * chore(firebase): send id too
    
    * chore(package, yarn): remove uniqid
    
    * refactor(Guide, firebase): using document id for guides
    
    * chore(Guide.ts): remove autoimport

--- src/models/Guide.ts
+++ src/models/Guide.ts
@@ -2,9 +2,11 @@ import { Post } from "./Post";
 import { Tag } from "./Tag";
 
 export interface Guide {
+  id?: string;
   title: string;
   sections: Array<Post>;
   tags: Array<Tag>;
-  character?: string | null;
+  character: string | null;
   description?: string;
+  doc_ref?: any;
 }
