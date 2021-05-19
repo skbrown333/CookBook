0eef14960153dddfca2e6df72ff338b91d59bae3
skbrown333 <skbrown333@gmail.com>
2021-05-19 09:58:45 -0500

    chore(eslint): error on unused

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,6 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
 };
