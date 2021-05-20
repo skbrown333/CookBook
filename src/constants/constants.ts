a75c8daf78223dcec9f6e17665d71691e0361cd4
skbrown333 <skbrown333@gmail.com>
2021-05-19 23:12:48 -0500

    fix(constants): redirect url client id

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -29,7 +29,7 @@ export const FUNCTIONS = {
  */
 export const DISCORD = {
   authUrl: encodeURI(
-    `https://discord.com/api/oauth2/authorize?clientid=${process.env.REACT_APP_DISCORD_ID}&redirect_uri=${window.location.origin}/login&response_type=code&scope=identify email`,
+    `https://discord.com/api/oauth2/authorize?client_id=${process.env.REACT_APP_DISCORD_ID}&redirect_uri=${window.location.origin}/login&response_type=code&scope=identify email`,
   ),
   getAvatarUrl: function (id, avatar) {
     return `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`;
