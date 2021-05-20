e7ed80d25ef5e890bc2927c82321f8051ac4fd03
Collin <cjdekalb@Gmail.com>
2021-05-19 23:06:19 -0500

    [COOK-60] Posts (#41)
    * fix(GuideDetail, TagInput): new tags apply on save
    
    * chore(ListView, App): change filename, add route
    
    * refactor(GuideList, SearchCreate): Search own component
    
    * fix(App): no object error on logout
    
    * chore(constants): change id field
    
    * feat(App.tsx): add post route
    
    * chore(Post.ts): change id and add doc_ref
    
    * feat(PostList): add, edit, delete posts
    
    * chore(PostListView): remove extra
    
    * chore(PostList, SearchCreate): fixed styling

--- src/components/ListView/ListView.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-export interface ListViewProps {}
-
-export const ListView: FunctionComponent<ListViewProps> = () => {
-  return <div>list view</div>;
-};
