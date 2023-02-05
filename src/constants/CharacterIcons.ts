8954c532068acf82e5612fd5baf88eaaceb0537d
skbrown333 <skbrown333@gmail.com>
2023-02-05 11:31:11 -0800

    feat(pplus): add pplus assets

--- src/constants/CharacterIcons.ts
+++ src/constants/CharacterIcons.ts
@@ -1,5 +1,6 @@
 import { MELEE as melee } from './MeleeIcons';
 import { ULTIMATE as ultimate } from './UltimateIcons';
 import { STRIVE as strive } from './StriveIcons';
+import { PPLUS as pplus } from './PplusIcons';
 
-export const CHARACTERS = { melee, ultimate, strive };
+export const CHARACTERS = { melee, ultimate, strive, pplus };
---
+++ src/constants/PplusIcons.ts
@@ -0,0 +1,95 @@
+import bowser from '../images/pm/bowser.png';
+import donkey_kong from '../images/pm/donkeykong.png';
+import dr_mario from '../images/pm/drmario.png';
+import falco from '../images/pm/falco.png';
+import falcon from '../images/pm/falcon.png';
+import fox from '../images/pm/fox.png';
+import game_and_watch from '../images/pm/game&watch.png';
+import ganon from '../images/pm/ganondorf.png';
+import ice_climbers from '../images/pm/iceclimbers.png';
+import jiggly_puff from '../images/pm/jigglypuff.png';
+import kirby from '../images/pm/kirby.png';
+import link from '../images/pm/link.png';
+import luigi from '../images/pm/luigi.png';
+import mario from '../images/pm/mario.png';
+import marth from '../images/pm/marth.png';
+import mew_two from '../images/pm/mewtwo.png';
+import ness from '../images/pm/ness.png';
+import peach from '../images/pm/peach.png';
+import pichu from '../images/pm/pichu.png';
+import pikachu from '../images/pm/pikachu.png';
+import roy from '../images/pm/roy.png';
+import samus from '../images/pm/samus.png';
+import sheik from '../images/pm/sheik.png';
+import yoshi from '../images/pm/yoshi.png';
+import young_link from '../images/pm/younglink.png';
+import zelda from '../images/pm/zelda.png';
+import charizard from '../images/pm/charizard.png';
+import diddy_kong from '../images/pm/diddykong.png';
+import ike from '../images/pm/ike.png';
+import ivysaur from '../images/pm/ivysaur.png';
+import knuckles from '../images/pm/knuckles.png';
+import dedede from '../images/pm/dedede.png';
+import lucario from '../images/pm/lucario.png';
+import lucas from '../images/pm/lucas.png';
+import meta_knight from '../images/pm/metaknight.png';
+import olimar from '../images/pm/olimar.png';
+import pit from '../images/pm/pit.png';
+import rob from '../images/pm/rob.png';
+import snake from '../images/pm/snake.png';
+import sonic from '../images/pm/sonic.png';
+import squirtle from '../images/pm/squirtle.png';
+import wario from '../images/pm/wario.png';
+import wolf from '../images/pm/wolf.png';
+import zero_suit from '../images/pm/zerosuit.png';
+import wireframe from '../images/melee/wireframe.png';
+import sandbag from '../images/melee/sandbag.png';
+
+export const PPLUS = {
+  charizard,
+  diddy_kong,
+  ike,
+  ivysaur,
+  knuckles,
+  dedede,
+  lucario,
+  lucas,
+  meta_knight,
+  olimar,
+  pit,
+  rob,
+  snake,
+  sonic,
+  squirtle,
+  wario,
+  wolf,
+  zero_suit,
+  bowser,
+  donkey_kong,
+  dr_mario,
+  falco,
+  falcon,
+  fox,
+  game_and_watch,
+  ganon,
+  ice_climbers,
+  jiggly_puff,
+  kirby,
+  link,
+  luigi,
+  mario,
+  marth,
+  mew_two,
+  ness,
+  peach,
+  pichu,
+  pikachu,
+  roy,
+  samus,
+  sheik,
+  yoshi,
+  young_link,
+  zelda,
+  wireframe,
+  sandbag,
+};
